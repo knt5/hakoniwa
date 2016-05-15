4579b6270a9d41b723df06c4bd30a15616b4fbc0
knt5 <knt5@users.noreply.github.com>
2016-05-16 01:05:37 +0900

    Refactor hakoniwa.js

--- src/js/index/hakoniwa.js
+++ src/js/index/hakoniwa.js
@@ -12,24 +12,10 @@ var hakoniwa = (function() {
 	// work canvas config
 	var work = config.work;
 	
-	/*
-	// Lat lng
-	var lat = dsm.startLat;
-	var lng = dsm.startLng;
-	
-	//
-	for(y = 0; y<dsm.image.height; y++) {
-		for(x = 0; x<dsm.image.width; x++) {
-			lng = dsm.startLng + dsm.pixelSize * x;
-		}
-		lat = dsm.startLat + dsm.pixelSize * y;
-	}
-	*/
-	
 	function isReady() {
 		if (
-			config.dsm.image &&
-			config.dsm.maskImage
+			dsm.image &&
+			dsm.maskImage
 		) {
 			return true;
 		}
@@ -41,22 +27,18 @@ var hakoniwa = (function() {
 			return;
 		}
 		
-		// copy
+		// Copy to work canvas
 		image = dsmContext.getImageData(0, 0, work.width, work.height);
 		workContext.fillStyle = '#000';
 		workContext.fillRect(0, 0, work.width, work.height);
 		workContext.putImageData(image, 0, 0);
-		console.log(image);
+		
+		// draw maps
+		
+		
+		
+		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	return {
 		init: init
