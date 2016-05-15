754add964d77e19d6d9c02989f86de88e07e7344
knt5 <knt5@users.noreply.github.com>
2016-05-15 22:14:09 +0900

    Remove geotiff.js

--- src/js/index/main.js
+++ src/js/index/main.js
@@ -5,9 +5,19 @@
 var $window = $(window);
 var $canvas = $('#canvas');
 var $map = $('#map');
+var $message = $('#message');
 
 // Google maps map object
 var map;
+
+// Config
+var config = {
+	// GeoTiff
+	defaultGeoTiff: 'assets/dsm/N035E139.tif',
+	
+	// Default GeoTiff mask
+	defaultGeoTiffMask: 'assets/dsm/N035E139.mask.tif'
+};
 
 //=====================================================================
 // Event handler
@@ -23,3 +33,32 @@ $window.on('resize', onResize);
 
 // Init 3D canvas div size
 onResize();
+
+//=====================================================================
+// Load default GeoTiff from online
+$.ajax(config.defaultGeoTiff, {
+	success: function(data) {
+		
+		/*
+		var tiff = GeoTIFF.parse(data);
+		var image = tiff.getImage();
+		var rasters = image.readRasters(function() {
+			var canvas = document.getElementById('plot');
+			var plot = new plotty.plot({
+				canvas: canvas,
+				data: rasters[0],
+				width: image.getWidth(),
+				height: image.getHeight(),
+				domain: [0, 256],
+				colorScale: 'viridis'
+			});
+			plot.render();
+		});
+		*/
+		//console.log(image);
+		
+	},
+	error: function() {
+		$message.text('Load failed. Please reload.');
+	}
+});
