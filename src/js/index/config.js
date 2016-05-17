7356e8a8a9735d190dfe97b8d752688066d2add1
knt5 <knt5@users.noreply.github.com>
2016-05-18 00:14:47 +0900

    Update hakoniwa core

--- src/js/index/config.js
+++ src/js/index/config.js
@@ -4,15 +4,25 @@ var config = {
 	dsm: {
 		path: 'assets/dsm/N035E139.tokyo.png',
 		maskPath: 'assets/dsm/N035E139.tokyo.mask.png',
-		startLng: 139.68,
-		startLat: 35.61,
+		north: 35.76,
+		east: 139.81,
+		south: 35.61,
+		west: 139.68,
 		pixelSize: 0.000277777778,
 		image: null,
 		maskImage: null
 	},
 	work: {
-		width: 100,
-		height: 100
+		width: 50,
+		height: 50,
+		//north: 35.68668700382425,
+		//west: 139.75879518270835
+		//north: 35.66660539785998,
+		//west: 139.69699793820973
+		//north: 35.693097686977694,
+		//west: 139.68051772242552
+		north: 35.633678619189524,
+		west: 139.72721500523392
 	},
 	cube: {
 		size: 3
--- src/js/index/hakoniwa.js
+++ src/js/index/hakoniwa.js
@@ -1,7 +1,7 @@
 //=====================================================================
 // Hakoniwa
 
-var hakoniwa = (function(parent) {
+var hakoniwa = (function() {
 	
 	//-----------------------------------------------------------------
 	// var
@@ -12,13 +12,45 @@ var hakoniwa = (function(parent) {
 	var work = config.work;
 	
 	// three.js
-	var camera, scene, renderer;
+	var scene;
+	var trackball;
+	var pointLights = [];
+	var pointLightRadius = 70;
+	//var cubeCamera;
+	
+	// camera position
+	var radius = 130;
+	var radian = -1;
+	//var theta = 0;
+	
+	// cubes
+	var cubes = [];
+	
+	// auto pilot status
+	var autoPilot = {
+		enabled: true,
+		up: false,
+		down: false,
+		right: true,
+		left: false,
+		turned: {
+			north: 0,
+			west: 0
+		},
+		reachedTop: false,
+		prevLeft: false,
+		speed: 1
+	};
+	
+	// height scale
+	var heightSclae = 4;
 	
 	//-----------------------------------------------------------------
 	function isReady() {
 		if (
 			dsm.image &&
-			dsm.maskImage
+			dsm.maskImage &&
+			typeof google !== 'undefined'
 		) {
 			return true;
 		}
@@ -26,80 +58,390 @@ var hakoniwa = (function(parent) {
 	}
 	
 	//-----------------------------------------------------------------
-	function init() {
-		var x, y;
-		var image;
+	//
+	
+	// Get work x on dsm canvas
+	function getWorkDsmX() {
+		return (work.west - dsm.west) / dsm.pixelSize;
+	}
+	
+	// Get work y on dsm canvas
+	function getWorkDsmY() {
+		return (dsm.north - work.north) / dsm.pixelSize;
+	}
+	
+	//-----------------------------------------------------------------
+	function getElevationColor(elevation, water) {
+		var colors = [
+			// green
+			0x98fb98,
+			0x9acd32,
+			0x32cd32,
+			
+			// orange
+			//0xf5deb3,
+			0xdeb887,
+			0xdaa520,
+			0xcd853f,
+			0xd2691e,
+			0xa0522d,
+			0x8b4513,
+			0x800000
+		];
 		
-		//-------------------------------------------------
-		if (!isReady()) {
-			return;
+		var minElevation = 20;
+		var step = 10;
+		
+		if (water !== 0) {
+			return 0x38a1db;
+		} else {
+			for (var i = 0; i<colors.length; i++) {
+				if (elevation < i * step + minElevation) {
+					return colors[i];
+				}
+			}
+			return 0x800000;
 		}
-		
-		//-------------------------------------------------
-		// Copy to work canvas
-		image = dsmContext.getImageData(0, 0, work.width, work.height);
+	}
+	
+	//-----------------------------------------------------------------
+	// Copy from dsm canvas to work canvas
+	function updateWorkCanvas() {
+		var image = dsmContext.getImageData(getWorkDsmX(), getWorkDsmY(), work.width, work.height);
 		workContext.fillStyle = '#000';
 		workContext.fillRect(0, 0, work.width, work.height);
 		workContext.putImageData(image, 0, 0);
 		
+		if (dsm.maskImage) {
+			var maskImage = maskContext.getImageData(getWorkDsmX(), getWorkDsmY(), work.width, work.height);
+			workMaskContext.fillStyle = '#000';
+			workMaskContext.fillRect(0, 0, work.width, work.height);
+			workMaskContext.putImageData(maskImage, 0, 0);
+		}
+	}
+	
+	//-----------------------------------------------------------------
+	// Init & Create cubes
+	function createCubes() {
+		var x, y;
+		
+		// init cubes
+		cubes = [];
+		
+		// create mesh
+		for (y=0; y<work.height; y++) {
+			cubes.push([]);
+			for (x=0; x<work.width; x++) {
+				// Create cube
+				//var geometry = new THREE.CubeGeometry(size, size, size);
+				var geometry = new THREE.BoxGeometry(size, size, size);
+				var material = new THREE.MeshStandardMaterial();
+				//var material = new THREE.MeshBasicMaterial({envMap: cubeCamera.renderTarget});
+				var mesh = new THREE.Mesh(geometry, material);
+				
+				// Avoid auto update
+				mesh.matrixAutoUpdate = false;
+				mesh.rotationAutoUpdate = false;
+				
+				// position
+				mesh.position.x = (x - work.width / 2) * size;
+				mesh.position.z = (y - work.height / 2) * size;
+				
+				// add
+				scene.add(mesh);
+				
+				// add cubes
+				cubes[y].push(mesh);
+			}
+		}
+	}
+	
+	//-----------------------------------------------------------------
+	// Update style of cubes
+	function updateCubeStyle() {
+		var x, y;
+		var workImageData = workContext.getImageData(0, 0, work.width, work.height);
+		var workMaskImageData = workMaskContext.getImageData(0, 0, work.width, work.height);
+		for (y=0; y<work.height; y++) {
+			for (x=0; x<work.width; x++) {
+				// Get elevation
+				var elevation = workImageData.data[x * 4 + y * work.width * 4];
+				
+				// Get water status
+				var water = workMaskImageData.data[x * 4 + y * work.width * 4];
+				
+				// Get cube mesh
+				var mesh = cubes[y][x];
+				
+				// Set color
+				mesh.material.color.setHex(getElevationColor(elevation, water));
+				
+				// Set scale
+				//mesh.geometry.parameters.height = elevation / 3 * (heightSclae * 0.1);
+				mesh.scale.y = elevation / 3 * (heightSclae * 0.1);
+				
+				// Set position
+				mesh.position.y = elevation / 2 * (heightSclae * 0.1);
+				
+				// Update
+				mesh.updateMatrix();
+			}
+		}
+	}
+	
+	//-----------------------------------------------------------------
+	function isPoorDevice() {
+		// TODO: check GPU, OS, ...
+		if ($stage.width() > 320) {
+			return false;
+		}
+		return true;
+	}
+	
+	//-----------------------------------------------------------------
+	// [public] Update hakoniwa
+	// if config.work is changed, call me.
+	// (memo: changeDsm())
+	function update() {
+		updateWorkCanvas();
+		updateCubeStyle();
+		mapUtil.updateWorkAreaPosition();
+	}
+	
+	//-----------------------------------------------------------------
+	// [public]
+	function init() {
+		if (!isReady()) {
+			return;
+		}
+		
+		// Add polygons to map
+		mapUtil.addPolygons();
+		
+		// Copy to work canvas
+		updateWorkCanvas();
+		
 		//-------------------------------------------------
-		// three.js
+		// [3D] Basic
 		
 		// scene
 		scene = new THREE.Scene();
+		scene.autoUpdate = false;
+		scene.fog = new THREE.Fog(0xffffff, 80, 400);
 		
 		// camera
 		camera = new THREE.PerspectiveCamera(45, $stage.width() / $stage.height(), 1, 1000);
-		camera.position.set(200, 200, 200);
+		camera.position.set(radius/1.3, radius/1.4, radius);
+		camera.lookAt(scene.position);
 		
 		// renderer
 		renderer = new THREE.WebGLRenderer();
 		renderer.setSize($stage.width(), $stage.height());
 		$stage.append(renderer.domElement);
 		
-		// light
-		var directionalLight = new THREE.DirectionalLight(0xffffff);
-		directionalLight.position.set(0, 0.7, 0.7);
-		scene.add(directionalLight);
+		// trackball
+		trackball = new THREE.TrackballControls(camera, $stage.get(0));
+		trackball.staticMoving = true;
+		trackball.dynamicDampingFactor = 1.0;
 		
 		//-------------------------------------------------
-		// Draw maps
-		for (y=0; y<work.height; y++) {
-			for (x=0; x<work.width; x++) {
-				
-				var geometry = new THREE.CubeGeometry(size, size, size);
-				var material = new THREE.MeshLambertMaterial({  // MeshPhongMaterial
-					color: 0xff0000
-				});
-				var mesh = new THREE.Mesh(geometry, material);
-				
-				mesh.position.x = x * size;
-				mesh.position.y = y * size;
-				mesh.position.z = 0;
-				mesh.rotation.x = Math.random();
-				mesh.rotation.y = Math.random();
-				mesh.rotation.z = Math.random();
-				
-				scene.add(mesh);
-			}
+		// [3D] Light
+		
+		// directionalLight
+		var directionalLight = new THREE.DirectionalLight(0xffffff);
+		directionalLight.position.set(0, 100, 20);
+		directionalLight.castShadow = true;
+		scene.add(directionalLight);
+		
+		// ambientLight
+		var ambientLight;
+		if (isPoorDevice()) {
+			ambientLight = new THREE.AmbientLight(0xffffff);
+			scene.add(ambientLight);
+		} else {
+			ambientLight = new THREE.AmbientLight(0x999999);
+			scene.add(ambientLight);
 		}
 		
-		// animate
+		// point light
+		if (!isPoorDevice()) {
+			var pointLightSphere = new THREE.SphereGeometry( 0.5, 16, 8 );
+
+			pointLights.push(new THREE.PointLight(0xff8000, 3, 150));
+			//pointLights[0].castShadow = true;
+			pointLights[0].position.set(pointLightRadius, 20, pointLightRadius);
+			pointLights[0].add(new THREE.Mesh(pointLightSphere, new THREE.MeshBasicMaterial({color: 0xff8000})));
+			scene.add(pointLights[0]);
+
+			pointLights.push(new THREE.PointLight(0xffff80, 2, 150));
+			//pointLights[1].castShadow = true;
+			pointLights[1].position.set(pointLightRadius, 20, pointLightRadius);
+			pointLights[1].add(new THREE.Mesh(pointLightSphere, new THREE.MeshBasicMaterial({color: 0xffff80})));
+			scene.add(pointLights[1]);
+		}
+		
+		//-------------------------------------------------
+		// Reflection
+		//cubeCamera = new THREE.CubeCamera( 1, 1000, 256 );
+		//cubeCamera.renderTarget.texture.minFilter = THREE.LinearMipMapLinearFilter;
+		//scene.add(cubeCamera);
+		
+		//-------------------------------------------------
+		// [3D] Helper
+		
+		// axis
+		var axisHelper = new THREE.AxisHelper(120);
+		scene.add(axisHelper);
+		
+		// grid
+		var gridHelper = new THREE.GridHelper(100, 5);
+		scene.add(gridHelper);
+		
+		// light shadow
+		//var directionalLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+		//scene.add(directionalLightShadowHelper);
+		
+		// light
+		var directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
+		scene.add(directionalLightHelper);
+		
+		//-------------------------------------------------
+		// [3D] Mesh
+		
+		// Create cubes
+		createCubes();
+		
+		// Set cube style
+		updateCubeStyle();
+		
+		//-------------------------------------------------
+		// [3D] Start animation
 		animate();
 	}
 	
 	//-----------------------------------------------------------------
-	function animate() {
-		requestAnimationFrame(animate);
+	// Auto pilot
+	function updateWorkAreaPositionByAutoPilot() {
+		if (!autoPilot.enabled) {
+			return;
+		}
 		
-		camera.rotation.z += 0.01;
+		if (autoPilot.up) {
+			work.north += dsm.pixelSize * autoPilot.speed;
+			
+			// reached dsm top
+			if (work.north >= dsm.north) {
+				autoPilot.reachedTop = true;
+			}
+			
+			// moved enough
+			if (work.north >= dsm.north ||
+				work.north >= autoPilot.turned.north + dsm.pixelSize * work.height
+			) {
+				autoPilot.up = false;
+				work.north -= dsm.pixelSize * autoPilot.speed;
+				
+				if (autoPilot.prevLeft) {
+					autoPilot.right = true;
+					autoPilot.prevLeft = false;
+				} else {
+					autoPilot.left = true;
+					autoPilot.prevLeft = true;
+				}
+			}
+			
+		} else if (autoPilot.down) {
+			work.north -= dsm.pixelSize * autoPilot.speed;
+			
+			// moved enough
+			if (work.north - dsm.pixelSize * work.height <= dsm.south) {
+				autoPilot.down = false;
+				work.north += dsm.pixelSize * autoPilot.speed;
+				
+				if (autoPilot.prevLeft) {
+					autoPilot.right = true;
+					autoPilot.prevLeft = false;
+				} else {
+					autoPilot.left = true;
+					autoPilot.prevLeft = true;
+				}
+			}
+			
+		} else if (autoPilot.right) {
+			work.west += dsm.pixelSize * autoPilot.speed;
+			
+			if (work.west + dsm.pixelSize * work.width >= dsm.east) {
+				autoPilot.right = false;
+				work.west -= dsm.pixelSize * autoPilot.speed;
+				
+				if (autoPilot.reachedTop) {
+					autoPilot.down = true;
+					autoPilot.reachedTop = false;
+				} else {
+					autoPilot.up = true;
+				}
+				autoPilot.turned.north = work.north;
+				autoPilot.turned.west = work.west;
+			}
+			
+		} else if (autoPilot.left) {
+			work.west -= dsm.pixelSize * autoPilot.speed;
+			
+			if (work.west <= dsm.west) {
+				autoPilot.left = false;
+				work.west += dsm.pixelSize * autoPilot.speed;
+				
+				if (autoPilot.reachedTop) {
+					autoPilot.down = true;
+					autoPilot.reachedTop = false;
+				} else {
+					autoPilot.up = true;
+				}
+				autoPilot.turned.north = work.north;
+				autoPilot.turned.west = work.west;
+			}
+		}
+	}
+	
+	//-----------------------------------------------------------------
+	function render() {
+		//theta += 1;
+		//camera.position.x = radius * Math.sin(THREE.Math.degToRad(theta));
+		//camera.position.z = radius * Math.cos(THREE.Math.degToRad(theta));
+		//camera.lookAt(scene.position);
+		
+		if (pointLights.length >= 2) {
+			radian += 0.1;
+			pointLights[0].position.x = pointLightRadius * Math.sin(radian);
+			pointLights[0].position.y = pointLightRadius / 2 * (Math.sin(radian * 0.3) + 1);
+			pointLights[0].position.z = pointLightRadius * Math.cos(radian * 0.8);
+			pointLights[1].position.x = pointLightRadius * Math.sin(-radian * 0.9);
+			pointLights[1].position.y = pointLightRadius / 2 * (Math.sin(-radian * 0.2) + 1);
+			pointLights[1].position.z = pointLightRadius * Math.cos(-radian);
+		}
+		
+		updateWorkAreaPositionByAutoPilot();
+		update();
+		
+		//cubeCamera.updateCubeMap( renderer, scene );
+		
+		trackball.update();
+		
+		scene.updateMatrixWorld();
 		
 		renderer.render(scene, camera);
 	}
 	
 	//-----------------------------------------------------------------
+	function animate() {
+		requestAnimationFrame(animate);
+		render();
+	}
+	
+	//-----------------------------------------------------------------
 	return {
-		init: init
+		init: init,
+		update: update
 	};
 	
 })();
